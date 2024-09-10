f2b73938ef8ca2421f9041f5152f784bbef6ee0e
Yuta-Kojima-1118 <yutakojima1118@gmail.com>
2024-09-10 10:48:30 +0900

    db

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,11 +1,13 @@
-create table IF NOT EXISTS todo (
-    todoid   integer auto_increment primary key,
-    todoname varchar (255) not null
-);
-
 create table IF NOT EXISTS match (
     matchid integer auto_increment primary key,
     matchname varchar(15) not null,
     matchfeeling integer not null,
-    
+    matchurl varchar(150) not null
+)
+
+create table IF NOT EXISTS chat (
+    chatid integer auto_increment primary key,
+    chatname varchar(15) not null,
+    chatmessage varchar(200) not null,
+    chattime TIMESTAMP not null
 )
