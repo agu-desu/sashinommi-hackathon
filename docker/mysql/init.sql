4b4d47288cbc4b8e06b8920c445c2cdcbbe5b59c
Yuta-Kojima-1118 <yutakojima1118@gmail.com>
2024-09-10 10:13:17 +0900

    test

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -2,3 +2,10 @@ create table IF NOT EXISTS todo (
     todoid   integer auto_increment primary key,
     todoname varchar (255) not null
 );
+
+create table IF NOT EXISTS match (
+    matchid integer auto_increment primary key,
+    matchname varchar(15) not null,
+    matchfeeling integer not null,
+    
+)
